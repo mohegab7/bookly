d8e9baed6fe665e7b0bfc8b52c93c9bf1187b6ef
elqot_7 <hegab837@gmail.com>
2025-06-08 18:59:15 +0300

    book entity

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,11 @@
+class  BookEntity{
+  final String image;
+  final String title;
+  final String author;
+  final num rating;
+  final num price;
+
+  BookEntity(this.image, this.title, this.author, this.rating, this.price);
+
+  
+}
