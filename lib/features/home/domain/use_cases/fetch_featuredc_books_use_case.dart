caf81fd614c82cc818569a0272fb8a02c87459fd
elqot_7 <hegab837@gmail.com>
2025-06-20 06:46:48 +0300

    refactor to use case

--- lib/features/home/domain/use_cases/fetch_featuredc_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featuredc_books_use_case.dart
@@ -1,13 +1,22 @@
 import 'package:dartz/dartz.dart';
 import 'package:hellllllo/core/errors/failure.dart';
+import 'package:hellllllo/core/use_cases/use_case.dart';
 import 'package:hellllllo/features/home/domain/entities/book_entity.dart';
 import 'package:hellllllo/features/home/domain/repos/home_repo.dart';
 
-class FetchFeaturedcBooksUseCase {
+class FetchFeaturedcBooksUseCase extends UseCase<List<BookEntity>,NoPrama> {
   final HomeRepo homeRepo;
 
   FetchFeaturedcBooksUseCase(this.homeRepo);
-  Future<Either<failure,List<BookEntity>>>fetchFutureBooks(){
-    return homeRepo.fetchFutureBooks();
+  
+  @override
+  Future<Either<failure, List<BookEntity>>> call([NoPrama? prama]) async{
+    
+    return await homeRepo.fetchFutureBooks();
   }
+  
+  
+  
 }
+
+class NoPrama{}
