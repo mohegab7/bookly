5a46668174c2c6cef59416b914a11db5c4778fac
elqot_7 <hegab837@gmail.com>
2025-07-05 07:33:58 +0300

    Fetch featured books home repo impl

--- lib/features/home/data/repos/home_repos_impl.dart
+++ lib/features/home/data/repos/home_repos_impl.dart
@@ -1,13 +1,28 @@
 import 'package:dartz/dartz.dart';
 import 'package:hellllllo/core/errors/failure.dart';
+import 'package:hellllllo/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:hellllllo/features/home/data/data_sources/home_remote_data_source.dart';
 import 'package:hellllllo/features/home/domain/entities/book_entity.dart';
 import 'package:hellllllo/features/home/domain/repos/home_repo.dart';
 
 class HomeReposImpl extends HomeRepo {
+  final HomeLocalDataSource homeLocalDataSource;
+  final HomeRemoteDataSource homeRemoteDataSource;
+
+  HomeReposImpl(
+      {required this.homeLocalDataSource, required this.homeRemoteDataSource});
   @override
-  Future<Either<failure, List<BookEntity>>> fetchFutureBooks() {
-    // TODO: implement fetchFutureBooks
-    throw UnimplementedError();
+  Future<Either<failure, List<BookEntity>>> fetchFutureBooks() async {
+    try {
+      var bookslist = homeLocalDataSource.fetchFutureBooks();
+      if (bookslist.isNotEmpty) {
+        return right(bookslist);
+      }
+      var books = await homeRemoteDataSource.fetchFutureBooks();
+      return right(books);
+    }  catch (e) {
+      return left(failure());
+    }
   }
 
   @override
