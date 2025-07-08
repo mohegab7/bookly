cd9db95aea1add9116ac631ab62749c3b7cdc328
elqot_7 <hegab837@gmail.com>
2025-07-08 07:06:40 +0300

    Implement cubit  fetch featured books method

--- lib/features/home/presentation/manger/feature books cubit/feature_books_cubit.dart
+++ lib/features/home/presentation/manger/feature books cubit/feature_books_cubit.dart
@@ -1,6 +1,17 @@
 import 'package:bloc/bloc.dart';
+import 'package:hellllllo/features/home/domain/use_cases/fetch_featuredc_books_use_case.dart';
 import 'package:hellllllo/features/home/presentation/manger/feature%20books%20cubit/FeatureBooksStates.dart';
 
 class featurebookscubit extends Cubit<Featurebooksstates> {
-  featurebookscubit() : super(FeaturedBookInitial());
+  featurebookscubit(this.featuredcBooksUseCasel) : super(FeaturedBookInitial());
+  final FetchFeaturedcBooksUseCase featuredcBooksUseCasel;
+  Future<void> FetchFeatureBooks() async {
+    emit(FeaturedBookInitial());
+    var result = await featuredcBooksUseCasel.call();
+    result.fold((failure) {
+      emit(FeaturedBookFailure(failure.message));
+    }, (books) {
+      emit(FeaturedBookSuccess(books));
+    });
+  }
 }
